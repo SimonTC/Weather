ddd4122fd3cf855b6c16519794fd917d583c0cda
Simon Clement <simon.clement+git@gmail.com>
2016-08-22 22:05:15 +0300

    Move API-key out of code
    Yes it is stupid and yes I have created a new key.
    The old key can't be used for anything now.

--- Weather.py
+++ Weather.py
@@ -6,20 +6,23 @@ Created on Thu Oct 15 12:43:54 2015
 @author: Simon
 """
 
-from pyowm import OWM #Source: https://github.com/csparpa/pyowm
+from pyowm import OWM  # Source: https://github.com/csparpa/pyowm
 import argparse
 import datetime
+import json
 
-API_KEY = "d0cb5fa784383be285f4b7a637ea8168"
+with open('keys.json', 'r') as f:
+    key_dict = json.load(f)
+api_key = key_dict['api_key']
 
-#Parse arguments
+# Parse arguments
 parser = argparse.ArgumentParser(description='Show the current weather.')
 parser.add_argument("-l", "--location", help="The location where you want to know the weather", default="Tampere, FI")
 args = parser.parse_args()
 location = args.location
 
-#Collect info
-owm = OWM(API_key=API_KEY, language='en')
+# Collect info
+owm = OWM(API_key=api_key, language='en')
 observation = owm.weather_at_place(location)
 weather = observation.get_weather()
 temp = weather.get_temperature(unit='celsius')['temp']
@@ -28,10 +31,10 @@ forecaster = owm.three_hours_forecast(location)
 forecast = forecaster.get_forecast()
 location = observation.get_location().get_name()
 
-print ('')
-print ('Current weather in %s:' %(location))
-print ('Temperature: %.1f degrees celsius' %(temp))
-print ('Sky: %s' %(status))
+print('')
+print('Current weather in %s:' % (location))
+print('Temperature: %.1f degrees celsius' % (temp))
+print('Sky: %s' % (status))
 print('')
 print('Forecast:')
 for i in range(4):
@@ -42,8 +45,6 @@ for i in range(4):
     ).strftime('%Y-%m-%d %H:%M')
     status = w.get_status()
     temp = w.get_temperature(unit='celsius')['temp']
-    print ('%s %s %.1f degrees' %(time_local, status, temp))
+    print('%s %s %.1f degrees' % (time_local, status, temp))
 
 print('')
-
-
